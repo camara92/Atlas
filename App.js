e1311097338f1d022ad730585844cf6f16f6abff
camara92 <88945535+camara92@users.noreply.github.com>
2022-07-04 01:35:11 +0200

    placeholder, textinput et affichage instantané

--- App.js
+++ App.js
@@ -1,50 +1,61 @@
 import React, {useState} from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, placeholder } from "react-native";
+
 //const name= "Daouda"; 
 // accessibilityLabel="Learn more about this purple button" : personne non voyant 
 // event avec btn 
 // input en react native 
 // et propriétés : placeholder, length, max, min 
+   {/* <TextInput style={styles.textInput} value={text}
+      onChangeText={text=> setText(text)} /> */}
 export default function App(){
  
-    const obj = {
-      name: "CAMARA",
-      age: 25
-    }
-    const [info,setInfo ]= useState(obj); 
-    const handlePress =()=>{
-      setInfo({
-        name:"Daouda", age: 27
-      }); 
-      
-    }
-    const [name, setName] =useState("Diallo"); 
+  const [name, setName] = useState(); 
+  const [prenom, setPrenom] = useState();
+  const [age, setAge] = useState();
    return(
     <View style={styles.wrapper}>
-      <Text style={styles.textFS20}>Nom : {info.name}  </Text>
-      <Text style={styles.textFS20}>Age : {info.age} </Text>
-      <Text style={styles.textFS20}> { name} </Text>
-      {/* <Button title="Cliquer ici " onPress={handlePress}/ > */}
-
-      <Button title="Cliquer ici " onPress={ ()=> setInfo({
-        name:"KingLife", age:25
-        
-      })}/ >
-   
+      
+   <Text style={styles.text}>Nom : {name}</Text>
+   <TextInput placeholder="Taper votre nom : CAMARA " style={styles.textInput}
+      onChangeText={val=> setName (val)} />
+      {/* prenom */}
+      <Text style={styles.text}>Prénom : {prenom}</Text>
+   <TextInput placeholder="Taper votre prénom : Daouda " style={styles.textInput}
+      onChangeText={valPre=> setPrenom (valPre)} />
+      {/* age  */}
+      <Text style={styles.text}>Age : {age}</Text>
+   <TextInput placeholder="Taper votre age : 25 " style={styles.textInput}
+      onChangeText={val=> setAge (val)} />
+      
     </View>
-   )
+   );
   
-  };
+  }
  
 
   const styles = StyleSheet.create({
     wrapper: {
-      marginTop:80, flexDirection: "column"
+      marginTop:50, 
+      // backgroundColor: "red", 
+      alignItems: "center", 
+      height: 200
     }, 
     textOne: {fontFamily:"Cochin"}, 
     textTwo: {fontFamily:"Arial", fontWeight: "bold", color: "white"}, 
     textThree: {fontFamily:"Times New Roman", color: "red"},
     view1: {backgroundColor: "green"}, 
     view2: {backgroundColor: "blue"},
-    textFS20: {fontSize: 20}
+    textFS20: {fontSize: 20}, 
+    textInput: {
+      height:40, 
+      borderColor:"grey" ,
+      borderWidth:1,
+      padding: 10, 
+      margin:9,
+  width:"90%"
+    }, text:{
+      fontSize: 20, 
+      
+    }
   })
--- archives_codes.js
+++ archives_codes.js
@@ -214,3 +214,64 @@ export default function App(){
     view2: {backgroundColor: "blue"},
     textFS20: {fontSize: 20}
   })
+
+
+
+
+
+
+
+
+
+  **************fin hook use*************
+
+  import React, {useState} from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+//const name= "Daouda"; 
+// accessibilityLabel="Learn more about this purple button" : personne non voyant 
+// event avec btn 
+// input en react native 
+// et propriétés : placeholder, length, max, min 
+export default function App(){
+ 
+    const obj = {
+      name: "CAMARA",
+      age: 25
+    }
+    const [info,setInfo ]= useState(obj); 
+    const handlePress =()=>{
+      setInfo({
+        name:"Daouda", age: 27
+      }); 
+      
+    }
+    const [name, setName] =useState("Diallo"); 
+   return(
+    <View style={styles.wrapper}>
+      <Text style={styles.textFS20}>Nom : {info.name}  </Text>
+      <Text style={styles.textFS20}>Age : {info.age} </Text>
+      <Text style={styles.textFS20}> { name} </Text>
+      {/* <Button title="Cliquer ici " onPress={handlePress}/ > */}
+
+      <Button title="Cliquer ici " onPress={ ()=> setInfo({
+        name:"KingLife", age:25
+        
+      })}/ >
+   
+    </View>
+   )
+  
+  };
+ 
+
+  const styles = StyleSheet.create({
+    wrapper: {
+      marginTop:80, flexDirection: "column"
+    }, 
+    textOne: {fontFamily:"Cochin"}, 
+    textTwo: {fontFamily:"Arial", fontWeight: "bold", color: "white"}, 
+    textThree: {fontFamily:"Times New Roman", color: "red"},
+    view1: {backgroundColor: "green"}, 
+    view2: {backgroundColor: "blue"},
+    textFS20: {fontSize: 20}
+  })
